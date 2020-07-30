35687ad082b2ddc3279892ba45ca4df78189ed67
dengxu <1084770933@qq.com>
2020-07-30 13:11:45 +0800

    分任务

--- src/main/java/com/css/demo/controller/LoginController.java
+++ src/main/java/com/css/demo/controller/LoginController.java
@@ -14,10 +14,23 @@ public class LoginController {
     @Autowired
     UserService userService ;
 
+    //首页
     @RequestMapping("/index")
     public String show(){
         return "console";
     }
+
+    //填写用户code弹窗
+    //初始化测评页面
+
+    //需求社区场景示例
+    //需求社区主页
+
+    //帖子内容页面
+    //问卷页
+    //结束页
+
+
 
     @RequestMapping(value = "/loginIn",method = RequestMethod.POST)
     public String login(String name , String password){
